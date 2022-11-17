86e04829ee2e9e40ba1bb64d077f952484294762
Alexander Gutnikov <zeratul477@gmail.com>
2022-11-17 16:47:55 +0100

    Add vert hor and percentile plots for density

--- Utilities/Metrics/GetDensityRadialAverage.m
+++ Utilities/Metrics/GetDensityRadialAverage.m
@@ -1,4 +1,4 @@
-function [densityRadialAverage, densityRadialStd, radiuses, isApproximated] = GetDensityRadialAverage(densityMatrix, coneDensityCentroid)
+function densityRadialDataStruct = GetDensityRadialAverage(densityMatrix, coneDensityCentroid, percents)
     % GetDensityRadialAverage returns density radial average starting in CDC point.
     % Based on Julius Ameln approach.
     %    - densityMatrix - matrix of density values. Matrix is the same size as the
@@ -6,10 +6,29 @@ function [densityRadialAverage, densityRadialStd, radiuses, isApproximated] = Ge
     %    - coneDensityCentroid - cone density centroid coordinates.
     %
     % Returns:
-    %    - densityRadialAverage - density radial average from CDC to the sides.
-    %    - radiuses - distances from CDC for each density radial average value.
-    %    - isApproximated - logical array. 1 - if NaN is in the calculation or
+    %    - densityRadialDataStruct - the struct with density radial average values.
+    %    Containts the next fields:
+    %       - densityRadialAverageAll - density radial average for the full circle.
+    %       - densityRadialStdAll - 
+    %       - radiusesAll - distances from CDC for each density radial average value.
+    %       - densityRadialAverageHorizontal - density radial average for the
+    %       top and bottom 10 degrees sectors.
+    %       - densityRadialStdHorizontal - 
+    %       - radiusesHorizontal - distances from CDC for each density radial average
+    %       value  for the top and bottom 10 degrees sectors.
+    %       - densityRadialAverageVertical - density radial average for the right and
+    %       left 10 degrees sectors.
+    %       - densityRadialStdVertical - 
+    %       - radiusesVertical - distances from CDC for each density radial average value the right and
+    %       left 10 degrees sectors.
+    %       - isApproximated - logical array. 1 - if NaN is in the calculation or
     %    distance is greater than the distance to the closest side of the image.
+
+    if ~exist("percents", "var")
+        percents = [10, 90];
+    else
+        percents = sort(percents);
+    end
 
     % find all relevant pixel in the matrix (not NaNs)
     [coordY, coordX] = find(~isnan(densityMatrix));
@@ -20,21 +39,57 @@ function [densityRadialAverage, densityRadialStd, radiuses, isApproximated] = Ge
     coordCDC_Y = coneDensityCentroid(2) - coordY;
 
     % calculate angle and distance towards cdc for every point
-    [~, radiuses] = cart2pol(coordCDC_X, coordCDC_Y);
+    [angles, radiuses] = cart2pol(coordCDC_X, coordCDC_Y);
     % round the distance to next whole number
     radiuses = round(radiuses);
+
+    % transfer radians to degree
+    angles = rad2deg(angles); 
+    % switch negative degree to full circle degree
+    angles(angles < 0) = 360 + angles(angles < 0); 
+
+    horizontalLine = (angles <= 185 & angles >= 175) | (angles <= 5 | angles >= 355) | radiuses == 0;
+    verticalLine = (angles <= 95 & angles >= 85) | (angles <= 275 & angles >= 265) | radiuses == 0;
+
+    densityRadialDataStruct = struct(...
+        'densityRadialAverageAll', [], 'densityRadialStdAll', [], 'radiusesAll', [], ...
+        'densityRadialAverageHorizontal', [], 'densityRadialStdHorizontal', [], 'radiusesHorizontal', [], ...
+        'densityRadialAverageVertical', [], 'densityRadialStdVertical', [], 'radiusesVertical', [], ...
+        'isApproximated', [], 'densityRadialPercentiles', [], 'percents', []);
+    [densityRadialDataStruct.densityRadialAverageAll,...
+        densityRadialDataStruct.densityRadialStdAll,...
+        densityRadialDataStruct.isApproximated,...
+        densityRadialDataStruct.radiusesAll,...
+        densityRadialDataStruct.densityRadialPercentiles] =...
+        CalculateRadialAvg(densityMatrix, coordinatesToAllPoints, radiuses, coneDensityCentroid, percents);
+    [densityRadialDataStruct.densityRadialAverageHorizontal,...
+        densityRadialDataStruct.densityRadialStdHorizontal,...
+        ~, ...
+        densityRadialDataStruct.radiusesHorizontal, ~] =...
+        CalculateRadialAvg(densityMatrix, coordinatesToAllPoints(horizontalLine, :), radiuses(horizontalLine), coneDensityCentroid, percents);
+    [densityRadialDataStruct.densityRadialAverageVertical,...
+        densityRadialDataStruct.densityRadialStdVertical,...
+        ~, ...
+        densityRadialDataStruct.radiusesVertical, ~] =...
+        CalculateRadialAvg(densityMatrix, coordinatesToAllPoints(verticalLine, :), radiuses(verticalLine), coneDensityCentroid, percents);
+    densityRadialDataStruct.percents = percents;
+end
+
+function [densityRadialAverage, densityRadialStd, isApproximated, radiuses, densityRadialPercentiles] = ...
+    CalculateRadialAvg(densityMatrix, coordinateToPoints, radiuses, coneDensityCentroid, percents)
 
     % get distance to closes side
     [dmHeight, dmWidth, ~] = size(densityMatrix);
     minDistanceToSide = min([coneDensityCentroid(1), coneDensityCentroid(2), dmHeight - coneDensityCentroid(2), dmWidth - coneDensityCentroid(1)]);
 
     % get coordinates by distance
-    coordinatesByDistance = arrayfun(@(a) coordinatesToAllPoints(radiuses == a,:), 0:max(radiuses), 'UniformOutput', false);
+    coordinatesByDistance = arrayfun(@(a) coordinateToPoints(radiuses == a,:), 0:max(radiuses), 'UniformOutput', false);
 
     % preallocate memory
     densityRadialAverage = nan(length(coordinatesByDistance), 1);
     densityRadialStd = nan(length(coordinatesByDistance), 1);
     isApproximated = false(length(coordinatesByDistance), 1);
+    densityRadialPercentiles = nan(length(percents), length(coordinatesByDistance));
 
     radiuses = unique(radiuses);
      
@@ -48,6 +103,8 @@ function [densityRadialAverage, densityRadialStd, radiuses, isApproximated] = Ge
         % calculate mean
         densityRadialAverage(iCoord) = mean(densityValuesToAverage(~isNaN));
         densityRadialStd(iCoord) = std(densityValuesToAverage(~isNaN));
+
+        densityRadialPercentiles(:, iCoord) = prctile(densityValuesToAverage(~isNaN), percents);
         % check if it is approximate
         isApproximated(iCoord) = any(isNaN) || radiuses(iCoord) > minDistanceToSide;
     end
